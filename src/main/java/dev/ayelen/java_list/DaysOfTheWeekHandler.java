a0fde5eda5b124b2e558f2698c4e6ba094f7da54
Ayuik <marcosayelen@gmail.com>
2025-01-31 15:30:30 +0100

    changed List to ArrayList

--- src/main/java/dev/ayelen/java_list/DaysOfTheWeekHandler.java
+++ src/main/java/dev/ayelen/java_list/DaysOfTheWeekHandler.java
@@ -1,5 +1,10 @@
 package dev.ayelen.java_list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DaysOfTheWeekHandler {
+    ArrayList<String> daysOfTheWeekList = new ArrayList <>();
+    
 
 }
