8f9b72463e36952957285aef871a651bf0821789
Ayuik <marcosayelen@gmail.com>
2025-01-31 16:22:03 +0100

    createList test passed

--- src/main/java/dev/ayelen/java_list/DaysOfTheWeekHandler.java
+++ src/main/java/dev/ayelen/java_list/DaysOfTheWeekHandler.java
@@ -7,6 +7,12 @@ public class DaysOfTheWeekHandler {
     ArrayList<String> daysOfTheWeekList = new ArrayList <>();
 
     public void createList() {
-        
+        daysOfTheWeekList.add("Monday");
+        daysOfTheWeekList.add("Tuesday");
+        daysOfTheWeekList.add("Wednesday");
+        daysOfTheWeekList.add("Thursday");
+        daysOfTheWeekList.add("Friday");
+        daysOfTheWeekList.add("Saturday");
+        daysOfTheWeekList.add("Sunday");
     }
 }
